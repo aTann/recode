ee5ddb885e0fd4c542fb7b26978f2b25585afed2
aTann <litfeature@163.com>
2022-04-04 00:15:07 +0800

    perf: 559

--- leetcode/simple/0559.js
+++ leetcode/simple/0559.js
@@ -21,25 +21,58 @@ var maxDepth = function (root) {
   }
 
   const { children } = root;
-  let layer = 1;
+  let depth = 1;
 
   if (children.length) {
-    let layers = Array(children.length);
+    let childrenDepths = Array(children.length);
     children.forEach((child, idx) => {
-      layers[idx] = maxDepth(child);
+      childrenDepths[idx] = maxDepth(child);
     });
-    layer += Math.max(...layers);
+    depth += Math.max(...childrenDepths);
   }
 
-  return 1 + Math.max(...layers);
+  return depth;
+};
+
+// 题解
+// 84ms/43.6MB
+var maxDepth = function (root) {
+  if (!root) {
+    return 0;
+  }
+
+  const { children } = root;
+  let maxChildDepth = 0;
+
+  if (children.length) {
+    children.forEach((child) => {
+      maxChildDepth = Math.max(maxDepth(child), maxChildDepth);
+    });
+  }
+
+  return maxChildDepth + 1;
 };
 
 // 广度优先
+// O(n)/O(max(layers))
+// 68ms/43.4MB
 var maxDepth = function (root) {
-  const count = 0;
-  let nodes = [root];
+  let count = 0;
+  if (root) {
+    let nodes = [root];
 
-  while (nodes.length) {
-    
+    while (nodes.length) {
+      const childList = [];
+      nodes.forEach((node) => {
+        if (node) {
+          const { children } = node;
+          childList.push(...children);
+        }
+      });
+      count += 1;
+      nodes = childList;
+    }
   }
+
+  return count;
 };
